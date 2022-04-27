e2b4df5ea06515f9c922d9c6b67018c0be746c47
Gyula Gombos <gombos88@gmail.com>
2022-04-27 20:22:50 +0200

    basic list design was created

--- app/src/main/java/com/agombiproducts/notes/MainActivity.kt
+++ app/src/main/java/com/agombiproducts/notes/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.agombiproducts.notes.database.Dummy
 import com.agombiproducts.notes.models.Note
 import com.agombiproducts.notes.ui.theme.NotesTheme
@@ -34,8 +39,18 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun NoteView(note: Note) {
-        Surface() {
-            Column() {
+        Surface(
+            modifier = Modifier
+                .padding(5.dp, 5.dp)
+                .fillMaxWidth(),
+            color = MaterialTheme.colors.secondary,
+            shape = RoundedCornerShape(17.dp)
+
+        ) {
+            Column(
+                modifier = Modifier.padding(5.dp, 20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 Text(text = note.task)
             }
 
